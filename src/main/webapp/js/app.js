0f5c969579757a39c336c3dce8f5f0782b086e4e
Jason Walker <JasonW94@gmail.com>
2018-08-15 02:02:50 -0500

    Got the rest of webpack & hot loader working.
    - There's still some loose in test stuff... everywhere...

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+
+const Greeter = props => (
+  <h2>Hello {props.name}</h2>
+);
 
 class App extends Component {
   render() {
     return(
       <div className="App">
-        <h1>Hello</h1>
+        <Greeter name="Steven" />
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
